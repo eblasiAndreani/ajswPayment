d87bc5c2f2a5238ba96e2877d58f7f56faef5724
Blasi Eduardo <eblasi@andreani.com>
2023-09-26 23:23:46 -0300

    First commit

---
+++ src/main/java/com/ajsw/ajswPayment/AjswPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.ajsw.ajswPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjswPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjswPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ajsw/ajswPayment/AjswPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajsw.ajswPayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjswPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
