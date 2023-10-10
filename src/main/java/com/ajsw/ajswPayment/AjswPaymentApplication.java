e427685e425854c60bace92972dc98f7e515f708
Lean Lima <lima.leandro1994@gmail.com>
2023-10-10 04:22:35 -0300

    Se agrega swagger y package controller

--- src/main/java/com/ajsw/ajswPayment/AjswPaymentApplication.java
+++ src/main/java/com/ajsw/ajswPayment/AjswPaymentApplication.java
@@ -1,13 +1,23 @@
 package com.ajsw.ajswPayment;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
 public class AjswPaymentApplication {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(AjswPaymentApplication.class);
+
 	public static void main(String[] args) {
+		LOGGER.info("the application is run.....");
 		SpringApplication.run(AjswPaymentApplication.class, args);
+		LOGGER.info("the application is now running.....");
 	}
 
 }
--- src/main/java/com/ajsw/ajswPayment/config/SwaggerConfig.java
+++ src/main/java/com/ajsw/ajswPayment/config/SwaggerConfig.java
@@ -1,37 +1,21 @@
 package com.ajsw.ajswPayment.config;
 
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
 @Configuration
-@Hidden
 public class SwaggerConfig {
-
-
-    @GetMapping("/")
-    public ResponseEntity<Void> redirectSwagger(HttpServletRequest request, HttpServletResponse response){
-        String url = "swagger-ui/index.html?configUrl=/v3/api-docs/swagger-config";
-        response.setHeader("Location", url);
-        response.setStatus(302);
-        return null;
-    }
-
 
     @Bean
     public OpenAPI customAPI(){
-        return new OpenAPI().info(new Info().title("TSHARE").version("1.0.0")
-                .license(new License().name("Lincença do Sistema").url("www.techdevbrazil.com"))
+        return new OpenAPI().info(new Info().title("Bar-Payment").version("1.0.0")
+                .license(new License().name("Lincencia de Sistemas").url("www.bar.com"))
         );
     }
+
+
 }
 
